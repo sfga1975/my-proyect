7600e430b267e95e1c91031fdb899dfbeef82d94
sfga1975 <sfga1975@gmail.com>
2026-09-09 11:35:40 +0200

    fcomp listas compresion

--- fcomp/main.py
+++ fcomp/main.py
@@ -1,6 +1,14 @@
 def run(xmin: int, xmax: int) -> list:
-    # TODO
-    return values
+    # xmin es el primer número que vamos a utilizar.
+    # xmax es el último número que vamos a utilizar.
+    # range crea una sucesión desde xmin hasta xmax.
+    # Sumamos 1 a xmax porque range no incluye su segundo límite.
+    # La expresión 3 * x + 2 aplica la función a cada número de la sucesión.
+    # La lista por comprensión reúne automáticamente todos los resultados.
+    valores = [3 * x + 2 for x in range(xmin, xmax + 1)]
+
+    # Devolvemos la lista completa de resultados.
+    return valores
 
 
 # DO NOT TOUCH THE CODE BELOW
